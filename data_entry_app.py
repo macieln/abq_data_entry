c8bbaf56685363433da90c8d484e48028671625e
Nahum Maciel <nahumamaciel@gmail.com>
2020-12-07 16:25:17 -0800

    Last commit before re image

--- data_entry_app.py
+++ data_entry_app.py
@@ -17,7 +17,12 @@ class Application(tk.Tk):
             csvwriter = csv.DictWriter(fh, fieldnames=data.keys())
             if newfile:
                 csvwriter.writeheader()
-                csvwriter.writerow(data)
+            csvwriter.writerow(data)
+        
+        self.records_saved += 1
+        self.status.set(
+            f"{self.records_saved} records saved this session.")
+        self.recordform.reset()
 
     def __init__(self, *args,**kwargs):
         super().__init__(*args, **kwargs)
@@ -42,6 +47,8 @@ class Application(tk.Tk):
         self.statusbar = ttk.Label(self, textvariable=self.status)
         self.statusbar.grid(sticky=(tk.W + tk.E), row=3, padx=10)
 
+        self.records_saved = 0
+        
 class LabelInput(tk.Frame):
     """A widget containing a label and input together."""
 
@@ -131,7 +138,7 @@ class DataRecordForm(tk.Frame):
         # Record Information
         recordinfo = tk.LabelFrame(self, text="Record Information")
 
-        self.inputs['Date'] = LabelInput(recordinfo, "Date", input_var=tk.StringVar)
+        self.inputs['Date'] = LabelInput(recordinfo, "Date", input_var=tk.StringVar())
         self.inputs['Date'].grid(row=0, column=0)
 
         self.inputs['Time'] = LabelInput(recordinfo, 
